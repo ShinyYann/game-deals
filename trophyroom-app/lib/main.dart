0bee527df896b636ec6976850083656abf9988eb
ShinyYann <shinyyann@ShinyYanndeMBP.lan>
2026-05-03 23:22:30 +0800

    fix: remove duplicate centerTitle

--- trophyroom-app/lib/main.dart
+++ trophyroom-app/lib/main.dart
@@ -558,7 +558,6 @@ class _HomePageState extends State<HomePage>
             );
           },
         ),
-        centerTitle: true,
         actions: [
           if (_netChecked)
             Padding(
